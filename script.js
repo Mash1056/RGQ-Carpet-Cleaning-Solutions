13da9a6a57c47f59cbb488444228885836f1883a
Mash1056 <matthewshepherd7@hotmail.com>
2025-11-18 16:21:46 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+---
+## ⚙️ `script.js`
+```javascript
+function sayHello() {
+alert("Thank you for choosing R.G.Q Carpet Cleaning Solutions!");
+}
+
+
+// Example placeholder for future scripts
+// function futureFeature() {
+// console.log("[[Future feature placeholder]]");
+// }
